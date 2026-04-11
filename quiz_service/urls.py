5a3b656be04bb045ff0d1a0fa85d2d000e480302
Andrey Tronov <pushkin@gmail.com>
2026-04-11 23:35:32 +0300

    [#24] Базовый шаблон приложения

--- quiz_service/urls.py
+++ quiz_service/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from quizzes.views import index
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +10,7 @@ urlpatterns = [
     path('quizzes/', include('quizzes.urls')),
     path('sessions/', include('quiz_sessions.urls')),
     path('stats/', include('quiz_stat.urls')),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('contacts/', TemplateView.as_view(template_name='contacts.html'), name='contacts'),
+    path('rules/', TemplateView.as_view(template_name='rules.html'), name='rules')
 ]
